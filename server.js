6e3f6ca05189a0aa4d955bb9c417613e7f8587e9
Lakshay Mahajan <67165004+lakshayMahajan@users.noreply.github.com>
2024-02-29 01:46:29 -0500

    added heroku support

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const userRoutes = require('./src/routes/UserRoutes');
 const productRoutes = require('./src/routes/productRoutes');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
@@ -18,7 +20,7 @@ mongoose
 .connect('mongodb+srv://admin:shelter@roary.m29hu3l.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 .then (() =>
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log('Server is listening on port 4000');
   })  
 )
